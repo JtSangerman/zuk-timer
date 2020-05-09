5486e919f85b9320cbf4c662f7119cc441e00e72
Jamie <jtsangerman@email.arizona.edu>
2020-05-08 17:39:00 -0700

    mat-button, progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from "./app.component";
 import { TimeFormatPipe } from "./pipes/time-format.pipe";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatButtonModule } from "@angular/material/button";
+import { MatSliderModule } from "@angular/material/slider";
 
 @NgModule({
   declarations: [AppComponent, TimeFormatPipe],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule
+  ],
   exports: [MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
